c578dcf93df187fc826effb29db01151bef210cc
hamma <hammamhiri@gmail.com>
2024-09-16 23:32:59 +0100

    Do basically everything

--- script.js
+++ script.js
@@ -14,70 +14,133 @@ function getComputerChoice() {
 }
 function getHumanChoice() {
     let ch = prompt('enter your choice');
-    ch=ch.toLowerCase()
+    ch = ch.toLowerCase()
     if (!(ch == 'rock' || ch == "scissors" || ch == 'paper')) {
         alert('try again');
         return
     }
     return ch
 }
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        let actualHuman = humanChoice.toLowerCase();
-        switch (actualHuman) {
-            case 'rock':
-                switch (computerChoice) {
-                    case 'rock':
-                        console.log('Tie!');
-                        break;
-                    case 'scissors':
-                        console.log('you win,rock beats scissors');
-                        ++humanScore;
-                        break;
-                    case 'paper':
-                        console.log('you lose,paper beats rock');
-                        ++computerScore;
-                        break;
-                }
-                break;
-            case 'paper':
-                switch (computerChoice) {
-                    case 'rock':
-                        console.log('you win,paper beats rock');
-                        ++humanScore;
-                        break;
-                    case 'scissors':
-                        console.log('you lose,scissors beats paper');
-                        ++computerScore;
-                        break;
-                    case 'paper':
-                        console.log('tie');
-                        break;
-                }
-                break;
-            case 'scissors':
-                switch (computerChoice) {
-                    case 'rock':
-                        console.log('you lose, rock beats scissors');
-                        ++computerScore;
-                        break;
-                    case 'scissors':
-                        console.log('tie');
-                        break;
-                    case 'paper':
-                        console.log('you win,scissors beats paper');
-                        ++humanScore;
-                        break;
-                }
-                break;
-
-        }
-
+let humanScore = {
+    value: 0
+};
+let computerScore = {
+    value: 0
+};
+var handler = {
+    set: function (obj, prop, value) {
+        obj[prop] = value;
+        score.textContent = `human : ${humanScore.value} computer : ${computerScore.value}`;
+        return true;
     }
-    for (let i=0;i<5;i++){
-        playRound(getHumanChoice(),getComputerChoice())
-    }
-    (humanScore>=computerScore) ? alert('you win!! '+humanScore+' to '+computerScore) : alert('you lose biiiiiitch '+humanScore+' to '+computerScore)
 }
+const score = document.querySelector('#score');
+const proxy1 = new Proxy(humanScore, handler);
+const proxy2 = new Proxy(computerScore, handler);
+score.textContent = `human : ${humanScore.value} computer : ${computerScore.value}`
+const lineBreak = document.createElement('br')
+
+function playRound(humanChoice, computerChoice) {
+    let actualHuman = humanChoice.toLowerCase();
+    let text = ""
+    let result = document.createTextNode(text)
+    const content = document.querySelector('#results')
+    switch (actualHuman) {
+        case 'rock':
+            switch (computerChoice) {
+                case 'rock':
+                    text = 'Tie!';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    break;
+                case 'scissors':
+                    text = 'you win,rock beats scissors';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    proxy1.value += 1;
+                    break;
+                case 'paper':
+                    text = 'you lose,paper beats rock';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    proxy2.value += 1;
+                    break;
+            }
+            break;
+        case 'paper':
+            switch (computerChoice) {
+                case 'rock':
+                    text = 'you win,paper beats rock';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    proxy1.value += 1;
+                    break;
+                case 'scissors':
+                    text = 'you lose,scissors beats paper';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    proxy2.value += 1;
+                    break;
+                case 'paper':
+                    text = 'tie';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    break;
+            }
+            break;
+        case 'scissors':
+            switch (computerChoice) {
+                case 'rock':
+                    text = 'you lose, rock beats scissors';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    proxy2.value += 1;
+                    break;
+                case 'scissors':
+                    text = 'tie';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    break;
+                case 'paper':
+                    text = 'you win,scissors beats paper';
+                    result = document.createTextNode(text);
+                    content.appendChild(result);
+                    content.appendChild(lineBreak.cloneNode(true));
+                    proxy1.value += 1;
+                    break;
+            }
+            break;
+
+    }
+    if (humanScore.value == 5) {
+        alert('you win')
+    };
+    if (computerScore.value == 5) {
+        alert('you lose bitch')
+    }
+
+}
+
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+function handleClick(choice) {
+    return function () {
+        const humanChoice = choice;
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+}
+rock.addEventListener('click', handleClick("rock"));
+paper.addEventListener('click', handleClick("paper"));
+scissors.addEventListener('click', handleClick("scissors"));
